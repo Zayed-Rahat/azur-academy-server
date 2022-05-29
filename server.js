9ed0638703c4d60cf1196247d085bea7dfb1ed33
Zayed-Rahat <azurahat129@gmail.com>
2022-05-30 02:25:17 +0600

    admin-protects-route

--- server.js
+++ server.js
@@ -23,16 +23,11 @@ mongoose
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
-/* 
+
 // routes middleware
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
-const port = process.env.PORT || 8000; */
-
-//step heroku
-/* if(process.env.NODE_ENV=="production"){
-  app.use(express.static("client/build"));
-} */
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
